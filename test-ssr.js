d1287f14711092089a4aaca8e7b5537de294725b
Gaurav Bhagchandani <gb@pykih.com>
2018-07-28 11:11:18 +0530

    Test SSR Fixed

--- test-ssr.js
+++ test-ssr.js
@@ -1,43 +1,99 @@
 var SSR = require('./dist/0.0.1/ssr-card.min.js')
 var state = {
     "dataJSON": {
-        "data": {
-            "section": "China to maintain communication with India on artificial lakes due to flooding concerns",
-            "title": "China to maintain communication with India on artificial lakes due to flooding concerns",
-            "links": [
-                {
-                    "link": "http://indianexpress.com/article/india/china-brahmaputra-india-4999314/",
-                    "favicon_url": "http://0.gravatar.com/blavatar/efe0300e7f891c5c802ed340f6b20b67?s=32",
-                    "publication_name": "TheIndianEXPRESS"
-                },
-                {
-                    "link": "http://indianexpress.com/article/india/pm-modi-didnt-question-manmohan-singh-ansaris-commitment-to-nation-arun-jaitley-in-rajya-sabha-5001071/",
-                    "favicon_url": "http://0.gravatar.com/blavatar/efe0300e7f891c5c802ed340f6b20b67?s=32",
-                    "publication_name": "TheIndianEXPRESS"
-                },
-                {
-                    "link": "http://indianexpress.com/article/india/parliament-winter-session-live-updates-congress-adjournment-motion-anantkumar-hegdes-constitution-remark-5000571/",
-                    "favicon_url": "http://0.gravatar.com/blavatar/efe0300e7f891c5c802ed340f6b20b67?s=32",
-                    "publication_name": "TheIndianEXPRESS"
-                },
-                {
-                    "link": "http://indianexpress.com/article/research/anant-kumar-hegde-secularism-constitution-india-bjp-jawaharlal-nehru-indira-gandhi-5001085/",
-                    "favicon_url": "http://0.gravatar.com/blavatar/efe0300e7f891c5c802ed340f6b20b67?s=32",
-                    "publication_name": "TheIndianEXPRESS"
-                },
-                {
-                    "link": "https://www.aninews.in/news/national/general-news/when-pm-suddenly-stopped-for-cup-of-coffee201712271531120001/",
-                    "favicon_url": "https://aniportalimages.s3.amazonaws.com/static/img/icons/apple-icon-120x120.png",
-                    "publication_name": "ANI"
-                },
-                {
-                    "link": "https://www.aninews.in/news/business/business/four-platforms-that-will-help-you-be-party-ready-this-new-year201712271523550001/",
-                    "favicon_url": "https://aniportalimages.s3.amazonaws.com/static/img/icons/apple-icon-120x120.png",
-                    "publication_name": "ANI"
-                }
-            ],
-            "analysis": "In a conversation with Indianexpress.com, [MacKinnon](https://www.googl.co.in) explained [how][2] the theory of [butterfly politics can be applied][3] to [India][4] and the [#MeToo campaign][5], [which she believes is one of the best illustrations of how butterfly interventions][6] produce social changes. She is the one."
-        }
+        "data":{
+            "bottom_links":[{
+              "text":"Policy",
+              "link":"#"
+            },{
+              "text":"Identity",
+              "link":"#"
+            },{
+              "text":"Politics",
+              "link":"#"
+            },{
+              "text":"Sanitation",
+              "link":"#"
+            },{
+              "text":"Deaths",
+              "link":"#"
+            }],
+            "branding_icon":"http://localhost:8080/images/project-logo.png",
+            "branding_name":"Sample Brand",
+            "branding_url":"#",
+            "company_icon":"http://localhost:8080/images/publisher-logo.png",
+            "company_name":"Sample Company",
+            "company_url":"#",
+            "copyright":"This material may not be published, broadcast, rewritten, or redistributed. ©2018 THE WIRE. All rights reserved.",
+            "description":"Manual Scavenging is a moral tragedy. It is also a man-made one. The manual scavenger is inextricably tied to the brutal realities and stubborn inequities of caste. Dr B.R. Ambedkar had pointed out that a scavenger is one not because of the job he does; he becomes one by his birth. The scavenger is very frequently also a She, a doubly invisibilised entity.",
+            "link_groups":[{
+              "heading":"Intersection",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"},
+                {"text":"Sanitation","link":"#"},
+                {"text":"Deaths","link":"#"}
+              ]
+            },{
+              "heading":"Intersectkghkghgjgjhjkgjhgjhion",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"},
+                {"text":"Sanitation","link":"#"},
+                {"text":"Deaths","link":"#"}
+              ]
+            },{
+              "heading":"Intersectkghkghgjgjhjkgjhgjhion",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"}
+              ]
+            },{
+              "heading":"Intersection",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"},
+                {"text":"Sanitation","link":"#"},
+                {"text":"Deaths","link":"#"}
+              ]
+            },{
+              "heading":"Intersection",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"}
+              ]
+            },{
+              "heading":"Intersection",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"},
+                {"text":"Sanitation","link":"#"},
+                {"text":"Deaths","link":"#"}
+              ]
+            },{
+              "heading":"Intersection",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"}
+              ]
+            },{
+              "heading":"Intersection",
+              "links":[
+                {"text":"Policy","link":"#"},
+                {"text":"Identity","link":"#"},
+                {"text":"Politics","link":"#"},
+                {"text":"Sanitation","link":"#"},
+                {"text":"Deaths","link":"#"}
+              ]
+            }]
+          }
     }
 }
 let x = SSR.render(state)
